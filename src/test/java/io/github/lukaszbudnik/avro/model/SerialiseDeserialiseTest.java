cb191a5d96993fb453d983c79a7e58bb0028b263
Łukasz Budnik <lukasz.budnik@gmail.com>
2015-01-03 22:22:03 +0100

    added new fields (removed in v2 schema), changed json to binary encoder/decoder

--- src/test/java/io/github/lukaszbudnik/avro/model/SerialiseDeserialiseTest.java
+++ src/test/java/io/github/lukaszbudnik/avro/model/SerialiseDeserialiseTest.java
@@ -10,37 +10,46 @@ import org.junit.Test;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.Arrays;
+import java.util.Random;
 
 public class SerialiseDeserialiseTest {
 
     @Test
-    public void shouldSerialiseDeserialiseTest() throws IOException {
+    public void shouldSerialiseDeserialiseAsBinaryTest() throws IOException {
+        byte[] array = new byte[1024];
+        new Random().nextBytes(array);
+
+        ByteBuffer buffer = ByteBuffer.wrap(array);
+
         TestCaseV1 testCaseV1 = TestCaseV1.newBuilder()
                 .setName("test case v1")
+                .setContent(buffer)
                 .build();
 
         TestRunV1 testRunV1 = TestRunV1.newBuilder()
                 .setName("test run v1")
+                .setConcurrencyLevel(2134567890)
                 .setTestCases(Arrays.asList(testCaseV1))
                 .build();
 
         DatumWriter<TestRunV1> writer = new SpecificDatumWriter<TestRunV1>(TestRunV1.getClassSchema());
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        Encoder out = EncoderFactory.get().jsonEncoder(TestRunV1.getClassSchema(), baos);
+        Encoder out = EncoderFactory.get().binaryEncoder(baos, null);
         writer.write(testRunV1, out);
         out.flush();
-        String json = baos.toString();
 
-        System.out.println(json);
+        System.out.println(baos.toByteArray().length);
 
         DatumReader<TestRunV2> reader = new SpecificDatumReader<TestRunV2>(TestRunV1.getClassSchema(), TestRunV2.getClassSchema());
         ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
-        Decoder in = DecoderFactory.get().jsonDecoder(TestRunV2.getClassSchema(), bais);
+        Decoder in = DecoderFactory.get().binaryDecoder(bais, null);
         TestRunV2 testRunV2 = reader.read(new TestRunV2(), in);
 
         Assert.assertEquals(testRunV1.getName(), testRunV2.getName());
         Assert.assertEquals(testRunV1.getTestCases(), testRunV2.getTestCases());
+        Assert.assertEquals(buffer, testRunV2.getTestCases().get(0).getContent());
         Assert.assertNull(testRunV2.getTimezone());
     }
 
